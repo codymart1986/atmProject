9718727c30f4ee9aac26c2080e9579b452e606a6
codymart1986 <codymart1986@hotmail.com>
2021-10-14 18:33:50 -0700

    working on function for atm.js

---
+++ account.js
@@ -0,0 +1,8 @@
+const pin = 1986
+
+const balance = 1000
+
+module.exports = {
+    pin, 
+    balance
+}
---
+++ atm.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')
+const { pin, balance} = require('./account');
+
+function getBalance(accountBalance){
+    return accountBalance
+}
+getBalance(balance)
+
+function 
